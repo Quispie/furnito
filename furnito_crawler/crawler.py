6588b9c641980bf4099dc0f20878817ac751421b
¨wb¨ <kingbolanda@live.com>
2016-03-09 23:33:13 +0100

    [#1] add log handler, add category crawler

--- furnito_crawler/crawler.py
+++ furnito_crawler/crawler.py
@@ -2,5 +2,20 @@ from __init__ import *
 
 class Crawler:
     def __init__(self):
-        pass
+        self.base_url = config.base_url
+
+    def get_categories(self):
+        '''
+        @usage: get categories of furnitures
+        @return: categories of furnitures
+        '''
+        try:
+            html = urlopen(self.base_url)
+        except HTTPError as e:
+            log.error_log("base_url unreachable")
+        
+        print self.base_url
+
+crawler = Crawler()
+crawler.get_categories()
 
