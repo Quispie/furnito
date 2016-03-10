614894f1cb068940709cf18e569d3727bb354095
¨wb¨ <kingbolanda@live.com>
2016-03-10 15:45:43 +0100

    [#1] add furniture instance

---
+++ furnito_crawler/furniture.py
@@ -0,0 +1,8 @@
+class Furniture:
+    def __init__(self, name, price, details, reviews):
+        self.name = name
+        self.price = price
+        self.details = details
+        self.reviews = reviews
+
+    
