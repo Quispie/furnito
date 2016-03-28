ab2afe3577c9827d60dd2b03ca2ff55d358175ce
= <kingbolanda@live.com>
2016-03-28 23:44:54 +0200

    [#7] improve efficiency

--- invert_index/file_writer.py
+++ invert_index/file_writer.py
@@ -5,6 +5,7 @@ class File_Writer:
     def __init__(self):
         self.temp_path = config.temp_path
         self.pl_path = config.posting_list_path
+        self.doc_len_path = config.doc_length_path
 
     def write_temp(self, content):
         '''
@@ -21,3 +22,11 @@ class File_Writer:
         '''
         with open(self.pl_path, 'w') as pl:
             json.dump(content, pl)
+
+    def write_doc_length(self, content):
+        '''
+        @usage: write document length to local storage
+        @arg content: content need to write
+        '''
+        with open(self.doc_len_path, 'w') as dl:
+            json.dump(content,dl)
